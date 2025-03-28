b696110e8c245dd515caa2414fe62d7e0f75a42d
Go-EunLee <koa789456@gmail.com>
2025-03-28 20:59:32 +0900

    순서 확인 테스트 진행

--- flow/src/test/java/com/waiting/flow/service/UserQueueServiceTest.java
+++ flow/src/test/java/com/waiting/flow/service/UserQueueServiceTest.java
@@ -128,4 +128,26 @@ class UserQueueServiceTest {
 
     }
 
+    @Test
+    void getRank() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.getRank("default", 100L))
+                )
+                .expectNext(1L)
+                .verifyComplete();
+
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 101L)
+                        .then(userQueueService.getRank("default", 101L))
+                )
+                .expectNext(2L)
+                .verifyComplete();
+    }
+
+    @Test
+    void emptyRank() {
+        StepVerifier.create(userQueueService.getRank("default", 100L))
+                .expectNext(-1L)
+                .verifyComplete();
+    }
+
 }
